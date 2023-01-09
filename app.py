8e7f38ecaafd7b35346485a0afe8dc05fb4210fb
Rordan1 <you@example.com>
2023-01-08 23:50:39 -0500

    first version

---
+++ app.py
@@ -0,0 +1,17 @@
+print("Hey come on in and get your tech")
+
+name2 = input("whats your name? \n")
+
+print("Howdy " + name2 +  " thanks for comin in.")
+
+products = ('cpus ' + 'gpus ' + "and " + 'software')
+
+print("in our store we offer " + products + "." + " would you care to buy anything? \n")
+
+purchase = input("I would like... \n")
+
+print("your " + purchase + " order will be ready in just a moment ")
+
+input();
+
+#%%
