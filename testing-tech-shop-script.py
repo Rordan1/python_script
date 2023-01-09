5299189cf46acf9f95d4febbc9cc0f5fe94df892
Rordan1 <you@example.com>
2023-01-09 00:15:14 -0500

    added spaces and price

---
+++ testing-tech-shop-script.py
@@ -0,0 +1,21 @@
+print("Hey come on in and get your tech")
+
+name2 = input("\n Whats your name? \n")
+
+print("Howdy " + name2 +  " thanks for comin in.")
+
+products = ('cpus ' + 'gpus ' + "and " + 'software')
+
+print("\n In our store we offer " + products + "." + " Would you care to buy anything? \n")
+
+order1 = input("I would like... \n" )
+
+price = str(300)
+
+print("\n Great, looks like we have " + order1 + " in stock, which will cost " + price + " dollares each")
+
+input("Please sign here if this amount is OK \n")
+
+print("Thanks " + name2 + " your order will be ready in just a moment ")
+
+input();
